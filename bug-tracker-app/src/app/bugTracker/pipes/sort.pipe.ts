76f7919a91778b9c659e1158794f36eed5784244
Magesh <tkmagesh77@gmail.com>
2019-01-03 17:15:00 +0530

    Day-02 Complete

--- bug-tracker-app/src/app/bugTracker/pipes/sort.pipe.ts
+++ bug-tracker-app/src/app/bugTracker/pipes/sort.pipe.ts
@@ -7,7 +7,7 @@ interface Comparer{
 @Pipe({
 	name : 'sort'
 })
-export class SortPipe implements PipeTransform{
+export class SortPipe implements PipeTransform {
 	private getComparerFor(attrName : string) : Comparer {
 		return function(p1 : any, p2 : any) : number {
 	        if (p1[attrName] < p2[attrName]) return -1;

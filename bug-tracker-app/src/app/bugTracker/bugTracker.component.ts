59bdff4d85d01ff1cd911b636b6c9356def18017
Magesh <tkmagesh77@gmail.com>
2019-01-03 14:14:33 +0530

    toggling the bug

--- bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
+++ bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
 
+/*
+class Bug{
+	constructor(public name : string, public isClosed : boolean = false){
+
+	}
+	toggle(){
+		this.isClosed = !this.isClosed;
+	}
+}
+*/
+
+interface Bug{
+	name : string,
+	isClosed : boolean
+}
+
 @Component({
 	selector : 'app-bug-tracker',
 	templateUrl : 'bugTracker.component.html'
 })
 export class BugTrackerComponent{
-	bugs : string[] = [];
+	bugs : Bug[] = [];
 
 	onAddNewClick(newBugName : string){
-		this.bugs.push(newBugName);
+		/* let newBug = new Bug(newBugName); */
+		let newBug : Bug = {
+			name : newBugName,
+			isClosed : false
+		} 
+		this.bugs.push(newBug);
+	}
+
+	onBugClick(bugToToggle : Bug){
+		bugToToggle.isClosed = !bugToToggle.isClosed;
 	}
 }

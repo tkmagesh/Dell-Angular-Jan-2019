3d842d0f25ca8d1d2d6c0acbf6dfe171c768ed03
Magesh <tkmagesh77@gmail.com>
2019-01-04 14:39:53 +0530

    Refactoring to Utils Module

--- bug-tracker-app/src/app/app.module.ts
+++ bug-tracker-app/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { UtilsModule } from './utils/utils.module';
+
 import { BugOperationsService } from './bugTracker/services/bugOperations.service';
 import { BugStorageService } from './bugTracker/services/bugStorage.service';
 import { AppEventService } from './bugTracker/services/appEvent.service';
@@ -10,9 +12,6 @@ import { AppComponent } from './app.component';
 import { BugTrackerComponent } from './bugTracker/bugTracker.component';
 
 import { ClosedCountPipe } from './bugTracker/pipes/closedCount.pipe';
-import { TrimTextPipe } from './bugTracker/pipes/trimText.pipe';
-import { SortPipe } from './bugTracker/pipes/sort.pipe';
-import { ElapsedPipe } from './bugTracker/pipes/elapsed.pipe';
 
 import { BugStatsComponent } from './bugTracker/views/bugStats.component';
 import { BugEditComponent } from './bugTracker/views/bugEdit.component';
@@ -24,14 +23,12 @@ import { BugEditComponent } from './bugTracker/views/bugEdit.component';
     , BugTrackerComponent
     , BugStatsComponent
     , BugEditComponent
-    , ClosedCountPipe
-    , TrimTextPipe
-    , SortPipe
-    , ElapsedPipe
+    , ClosedCountPipe    
   ],
   imports: [
     BrowserModule
     , FormsModule
+    , UtilsModule
   ],
   providers: [
   	BugOperationsService
